87cac6f2f9c400dcffc978d64e59658ff12d9493
Molokov Alex <sportmalex@yandex.ru>
2021-09-05 10:49:25 +0300

    hw1: reverse hello OTUS. Completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	const hello = "Hello, OTUS!"
+	fmt.Print(stringutil.Reverse(hello))
 }
