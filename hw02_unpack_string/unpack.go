56579d7720c8f2db1f56971fbf4d12a6680decb8
Molokov Alex <sportmalex@yandex.ru>
2021-09-12 20:22:12 +0300

    hw2: unpack func

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,47 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if len(str) == 0 {
+		return "", nil
+	}
+
+	result := strings.Builder{}
+	in := []rune(str)
+
+	if unicode.IsDigit(in[0]) {
+		return "", ErrInvalidString
+	}
+
+	isDigit := false
+	for i := 0; i < len(in); i++ {
+		if unicode.IsDigit(in[i]) {
+			if isDigit {
+				return "", ErrInvalidString
+			}
+			repeat, _ := strconv.Atoi(string(in[i]))
+			result.WriteString(strings.Repeat(string(in[i-1]), repeat))
+			isDigit = true
+			continue
+		}
+
+		if !isDigit && i > 0 {
+			result.WriteRune(in[i-1])
+		}
+
+		isDigit = false
+	}
+
+	if !isDigit {
+		result.WriteRune(in[len(in)-1])
+	}
+
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -9,13 +9,22 @@ import (
 
 func TestUnpack(t *testing.T) {
 	tests := []struct {
+		name     string
 		input    string
 		expected string
 	}{
-		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
-		{input: "aaa0b", expected: "aab"},
+		{name: "correct", input: "a4bc", expected: "aaaabc"},
+		{name: "correct", input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{name: "no number", input: "abccd", expected: "abccd"},
+		{name: "empty string", input: "", expected: ""},
+		{name: "digit is 0 ", input: "aaa0b", expected: "aab"},
+		{name: "one symbol", input: "a", expected: "a"},
+		{name: "repeat one time", input: "a1", expected: "a"},
+		{name: "capital", input: "A2s3K2", expected: "AAsssKK"},
+		{name: "cyrillic", input: "ы2я4", expected: "ыыяяяя"},
+		{name: "minus", input: "a-5", expected: "a-----"},
+		{name: "whitespace in", input: "a2 2", expected: "aa  "},
+		{name: "empty null digit", input: "a0", expected: ""},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -25,7 +34,7 @@ func TestUnpack(t *testing.T) {
 
 	for _, tc := range tests {
 		tc := tc
-		t.Run(tc.input, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, result)
@@ -34,7 +43,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "4"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
