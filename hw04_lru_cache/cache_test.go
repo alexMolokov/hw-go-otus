3506befa44a7165a742215569443e3475134d7bc
Molokov Alex <sportmalex@yandex.ru>
2021-10-10 12:24:50 +0300

    hw4: lru cache

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,7 +50,19 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(3)
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		c.Clear()
+		wasInCache = c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		_, ok := c.Get("bbb")
+		require.False(t, ok)
 	})
 }
 
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,88 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len   int
+	first *ListItem
+	last  *ListItem
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.first
+}
+
+func (l *list) Back() *ListItem {
+	return l.last
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	if i == l.first {
+		l.first = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	if i == l.last {
+		l.last = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	l.len--
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	i := &ListItem{Value: v, Next: l.first}
+
+	if l.len == 0 {
+		l.last = i
+	} else {
+		l.first.Prev = i
+	}
+
+	l.first = i
+	l.len++
+	return i
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	i := &ListItem{Value: v, Prev: l.last}
+
+	if l.len == 0 {
+		l.first = i
+	} else {
+		l.last.Next = i
+	}
+
+	l.last = i
+	l.len++
+	return i
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.first {
+		return
+	}
+
+	if i == l.last {
+		l.last = i.Prev
+		i.Prev.Next = nil
+	} else {
+		i.Next.Prev = i.Prev
+		i.Prev.Next = i.Next
+	}
+
+	i.Prev = nil
+	i.Next = l.first
+	l.first.Prev = i
+	l.first = i
 }
 
 func NewList() List {
