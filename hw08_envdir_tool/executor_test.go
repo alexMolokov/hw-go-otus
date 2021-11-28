f70ec5d996a807c64e44c82d5cce06406213f2bd
Molokov Alex <sportmalex@yandex.ru>
2021-11-28 23:30:06 +0300

    hw08: envdir executor tests

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestRunCmd(t *testing.T) {
-	t.Run("copy empty file", func(t *testing.T) {
-		require.True(t, true)
+	t.Run("set remove env", func(t *testing.T) {
+		_ = os.Setenv("DELETED", "deleted")
+		environment := make(map[string]EnvValue)
+		environment["HELLO_WORLD"] = EnvValue{Value: "hello world", NeedRemove: false}
+		environment["DELETED"] = EnvValue{Value: "", NeedRemove: true}
+
+		code := RunCmd([]string{"env"}, environment)
+		require.Equal(t, 0, code)
+		_, ok := os.LookupEnv("DELETED")
+		require.False(t, ok)
+		world, ok := os.LookupEnv("HELLO_WORLD")
+		require.True(t, ok)
+		require.Equal(t, "hello world", world)
+	})
+
+	t.Run("mkdir", func(t *testing.T) {
+		environment := make(map[string]EnvValue)
+
+		tempDir := os.TempDir()
+		dir := path.Join(tempDir, "bbb", "ccc")
+		defer func() {
+			_ = os.RemoveAll(path.Join(tempDir, "bbb"))
+		}()
+
+		code := RunCmd([]string{"mkdir", "-p", dir}, environment)
+		require.Equal(t, 0, code)
+
+		fi, err := os.Stat(dir)
+		require.Nil(t, err)
+		require.True(t, fi.IsDir())
 	})
 }
